f0592ab9fce12017a63361320b6f27fdee7198cd
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-12-02 12:39:18 -0500

    more restructuring

---
+++ log/index.php
@@ -0,0 +1,7 @@
+<?php include('../header.php'); ?>
+
+<script src="/js/log.js"></script>
+
+<!-- Log will go here -->
+
+<?php include('../footer.php'); ?>
---
+++ scoreboard/index.php
@@ -0,0 +1,7 @@
+<?php include('../header.php'); ?>
+
+<script src="/js/scoreboard.js"></script>
+
+<!-- Scoreboard will go here -->
+
+<?php include('../footer.php'); ?>
