f590d8a938e13630f14f771c7588034e3c85c141
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-12-02 20:14:23 -0500

    can't define function separately in conditional statements

--- js/init.js
+++ js/init.js
@@ -1,39 +1,37 @@
 // We define a function showLoggedModule() differently
 // depending on whether the user is logged in or not.
 
-// If logged out...
-if (!LOGGED_IN || LOGGED_IN === 'false') {
-
-	// Function to log in that is called when
-	// the user clicks on the forthcoming login button
-	function logIn(error, user) {
-
-		// Overwrite global variables and set localStorage
-		LOGGED_IN = true;
-		localStorage.setItem('LOGGED_IN', true);
-		USER = user.id;
-		localStorage.setItem('USER', user.id);
-
-		// If the user is not already present in our DATA, add them
-		if (!DATA.child('users').child(USER)) {
-			DATA.child('users').child(USER).set({
-				first_name: user.first_name,
-				name: user.name
-			});
-		// otherwise, update their record (in case anything's changed)
-		} else {
-			DATA.child('users').child(USER).update({
-				first_name: user.first_name,
-				name: user.name
-			});
-		}
-
-		// Refresh the page
-		window.location.reload();
-	}
-
+function showLoggedModule(module) {
 	// Here showLoggedModule just needs to show the login button
-	function showLoggedModule(module) {
+	if (!LOGGED_IN || LOGGED_IN === 'false') { 
+
+		// Function to log in that is called when
+		// the user clicks on the forthcoming login button
+		function logIn(error, user) {
+
+			// Overwrite global variables and set localStorage
+			LOGGED_IN = true;
+			localStorage.setItem('LOGGED_IN', true);
+			USER = user.id;
+			localStorage.setItem('USER', user.id);
+
+			// If the user is not already present in our DATA, add them
+			if (!DATA.child('users').child(USER)) {
+				DATA.child('users').child(USER).set({
+					first_name: user.first_name,
+					name: user.name
+				});
+			// otherwise, update their record (in case anything's changed)
+			} else {
+				DATA.child('users').child(USER).update({
+					first_name: user.first_name,
+					name: user.name
+				});
+			}
+
+			// Refresh the page
+			window.location.reload();
+		}
 
 		var logButton = document.createElement('button');
 		logButton.onclick = function(){
@@ -43,14 +41,11 @@ if (!LOGGED_IN || LOGGED_IN === 'false') {
 		logButton.innerHTML = 'Log in';
 
 		module.appendChild(logButton);
-	}
 
-// If user is logged in...
-} else {
-
-	// In this case, showLoggedModule is a little more complicated.
+	// If logged in, showLoggedModule is a little more complicated.
 	// We need a logout button (that, when clicked, resets globals and localStorage)
-	function showLoggedModule(module) {
+	} else {
+		
 		var logButton = document.createElement('button');
 		logButton.innerHTML = 'Log out';
 		logButton.onclick = function() {
